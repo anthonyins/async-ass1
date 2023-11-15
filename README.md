76d9cbe4f8d8fbe8721e32ec2559716bea5233d8
anthonyins <132161891+anthonyins@users.noreply.github.com>
2023-11-15 22:39:56 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# async-ass1
+# Random Cat Images
+
+# Beklenenler
+- Sayfa açıldıktan 3sn sonra **loadingDiv** ekrandan kaybolsun **containerDiv** görünür hale gelsin.
+- Aşağıda verilen api adresine fetch() metodunu kullanarak istek atınız. Gelen cevabı **cardDiv** içerisine yazdırınız.
+- Sayfa yüklendiğinde resimler ekrana gelsin. Aynı şekilde kullanıcı **btn** elementine tıkladığında da istek atılsın resimler yenilensin.
+- Eğer istekte hata meydana gelirse **cardDiv** içerisinde [hata resmi](./img/error.gif) ekrana gelsin.
+- Apiden response gelene kadar **cardDiv** içerisinde [laoding resmi](./img/loading.gif) ekrana gelsin.
+- **tarih** elementininin innerText ine anlık olarak tarih ve saat bilgisini yazdırınız.
+
+## Beklenen Çıktı
+
+![Cat](./cat.gif)
+
+## Not:
+1. Kullanacağınız API adresi => "https://api.thecatapi.com/v1/images/search?limit=10"
+2. Verilen templatei tercih etmeyip kendinize göre oluşturabilirsiniz.
---
+++ app.js
@@ -0,0 +1,54 @@
+const cardDiv = document.getElementById("cardDiv");
+const loadingDiv = document.getElementById("loading");
+const btn = document.querySelector(".btn");
+const containerDiv = document.querySelector(".container");
+const tarih = document.getElementById("tarih");
+
+const getCatImages = () => {
+  cardDiv.innerHTML = `<img src="./img/loading.gif" />`;
+
+  fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+    .then((res) => res.json())
+    .then((response) => {
+      createElem(response);
+    })
+    .catch((err) => {
+      errorMsg();
+    });
+};
+
+const createElem = (data) => {
+  cardDiv.innerHTML = "";
+  data.forEach((cat) => {
+    console.log(cat);
+    const elem = `<div class="col-12 col-sm-6 col-lg-4">
+
+      <div style="height:200px;">
+        <img src=${cat.url} class="w-100 h-100" alt="...">
+      </div>
+    </div>
+`;
+    cardDiv.innerHTML += elem;
+  });
+};
+
+const errorMsg = () => {
+  cardDiv.innerHTML = `<img src="./error.gif" />`;
+};
+
+const getTarih = () => {
+  setInterval(() => {
+    tarih.innerText = new Date().toLocaleString();
+  }, 1000);
+};
+window.addEventListener("load", () => {
+  containerDiv.style.display = "none";
+
+  setTimeout(() => {
+    containerDiv.style.display = "flex";
+    loadingDiv.style.display = "none";
+    getTarih();
+    getCatImages();
+  }, 3000);
+});
+btn.addEventListener("click", getCatImages);
